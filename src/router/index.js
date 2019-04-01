04ce8511fa2f3e418980fb7b6f6685ee92547e39
yangyuji <394712273@qq.com>
2019-04-01 10:12:40 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-const _import = require('./_import_' + process.env.NODE_ENV)
 Vue.use(Router)
 
-import Layout from '../views/layout/index'
+import Layout from '@/views/layout/index'
 
 export const constantRouterMap = [
   {
     path: '/login',
     name: 'login',
-    component: _import('login/index'),
+    component: import('@/views/login/index'),
     hidden: true
   },
   {
     path: '/404',
     name: '404',
-    component: _import('404'),
+    component: import('@/views/404'),
     hidden: true
   },
   {
